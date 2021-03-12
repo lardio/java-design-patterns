0fcde3e8e40d588e48fce576cf4f21c772ca9fb1
sylvain <lardiere.sylvain5@gmail.com>
2021-03-12 15:37:31 +0100

    v2

--- src/main/java/fr/diginamic/factory/EnceinteConnectee.java
+++ src/main/java/fr/diginamic/factory/EnceinteConnectee.java
@@ -1,10 +1,12 @@
 package fr.diginamic.factory;
 
 public class EnceinteConnectee extends ObjetConnecte {
+
     public EnceinteConnectee(Integer volt) {
         super(volt);
         this.nom = this.getClass().getSimpleName();
     }
 
     public EnceinteConnectee(){}
+
 }
--- src/main/java/fr/diginamic/factory/ObjetConnecteFactory.java
+++ src/main/java/fr/diginamic/factory/ObjetConnecteFactory.java
@@ -4,7 +4,6 @@ import java.lang.reflect.InvocationTargetException;
 public class ObjetConnecteFactory<T extends ObjetConnecte> {
 
     public static <T extends ObjetConnecte> T getObjetConnecte(ObjetConnecte objetConnecte) throws NoSuchMethodException, ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Class<?> act = Class.forName(objetConnecte.getNomTotal());
         Class<T> clazz = (Class<T>) Class.forName(objetConnecte.getNomTotal());
         T activityClass =  clazz.getDeclaredConstructor(Integer.class).newInstance(objetConnecte.getLimiteVolt());
         return activityClass;
